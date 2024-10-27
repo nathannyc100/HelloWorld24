f5f65989a550d9e8fd3347b596adf4fb8e250fb7
Nathan Yu <nathanyu@pal-nat186-86-172.itap.purdue.edu>
2024-10-27 10:21:51 -0400

    temp: trying to get color picker to work

--- src/screens/CameraPage.js
+++ src/screens/CameraPage.js
@@ -1,10 +1,13 @@
 import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import * as ImageManipulator from 'expo-image-manipulator';
 
 export default function CameraPage() {
   const [facing, setFacing] = useState('back');
   const [permission, requestPermission] = useCameraPermissions();
+  const [colorString, setColorString] = useState('test');
+  const cameraRef = useRef(undefined);
 
   if (!permission) {
     // Camera permissions are still loading.
@@ -22,12 +25,50 @@ export default function CameraPage() {
   }
 
   const takePicture = async () => {
-    const photo = await CameraView.takePictureAsync();
+    const photo = await cameraRef.current?.takePictureAsync();
+
+
+    const x = photo.width / 2;
+    const y = photo.width / 2;
+
+    setColorString(photo.uri);
+
+    const result = await ImageManipulator.manipulateAsync(
+      photo.uri,
+      [{ crop: { originX: x, originY: y, width: 1, height: 1 } }],
+      { base64: true }
+    );
+    
+    var getPixels = require("get-pixels")
+
+    getPixels(result, function(err, pixels) {
+      if(err) {
+        console.log("Bad image path")
+        return
+      }
+      console.log("got pixels", pixels.shape.slice())
+      setColorString(pixels)
+    })
+
+    return;
+
+    const base64 = result.base64;
+    const binaryString = atob(base64); // decode base64 to binary
+    const r = binaryString.charCodeAt(0);
+    const g = binaryString.charCodeAt(1);
+    const b = binaryString.charCodeAt(2);
+
+    setColorString(r + ", " + g + ", " + b);
+
+
+
+
   }
 
   return (
     <View style={styles.container}>
-      <CameraView style={styles.camera} facing={facing}>
+      <CameraView style={styles.camera} facing={facing} ref={cameraRef}>
+        <Text style={styles.colorText}>{colorString}</Text>
         <View style={styles.buttonContainer}>
           <TouchableOpacity style={styles.button} onPress={takePicture}>
             <Text style={styles.text}>Take Picture</Text>
@@ -66,5 +107,14 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: 'white',
   },
+  colorText: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+  }
 });
 
